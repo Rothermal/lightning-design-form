69b79dc913c2d042e17b5a2531472ebf254c1178
Mark Rothermal <rothermal2@comcast.net>
2017-07-10 15:58:26 -0500

    form updates

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Mark on 7/10/17.
+ */
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+
+router.get('/',function(request,response){
+    response.sendFile(path.join(__dirname,"../index.html"));
+});
+
+
+router.get('/*', function(request, response){
+    response.redirect('/');
+});
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Mark on 7/10/17.
+ */
+var express = require('express');
+var index = require('./routes/index');
+var app = express();
+var port =  process.env.PORT || 1985;
+
+
+app.use(express.static('./'));
+
+
+app.use('/',index);
+
+
+
+
+var server = app.listen(port,function(){
+    var port = server.address().port;
+    console.log('Serving Clients successfully since : ', port);
+});
